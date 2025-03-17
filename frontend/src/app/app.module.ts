279337b08c69084d4ebbb9a3135378be3c4cbf21
Suchitra-27 <139349673+Suchitra-27@users.noreply.github.com>
2025-03-17 18:58:18 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppComponent } from './app.component';
 import { GroupComponent } from './components/group/group.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     RouterModule.forRoot([]) // Ensure routing is enabled
   ],
   providers: [],
